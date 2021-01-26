f582d930fd48ecec4431586f6ea6fe8a1c23038b
moms-spaghetti <williamedwards36@aol.com>
2021-01-26 21:32:53 +0000

    fixes_#1

--- nandoku/src/components/menu/categories.js
+++ nandoku/src/components/menu/categories.js
@@ -2,37 +2,37 @@ import { flowers } from "./flowers";
 
 const categories = [
   {
-    link: { path: "/FlashcardPanel", data: flowers },
+    data: flowers,
     title: "花",
     myClass: "cat flowers",
     score: 0,
   },
   {
-    link: "/",
+    data: flowers,
     title: "鳥類",
     myClass: "cat birds",
     score: 0,
   },
   {
-    link: "/",
+    data: flowers,
     title: "色",
     myClass: "cat colours",
     score: 0,
   },
   {
-    link: "/",
+    data: flowers,
     title: "天気",
     myClass: "cat weather",
     score: 0,
   },
   {
-    link: "/",
+    data: flowers,
     title: "国名",
     myClass: "cat countries",
     score: 0,
   },
   {
-    link: "/",
+    data: flowers,
     title: "魚へん",
     myClass: "cat fish",
     score: 0,
