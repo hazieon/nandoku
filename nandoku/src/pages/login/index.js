bcf7b773d416af345f3ddca4f9c65e4fee29dd51
Hazieon <handrew3721@gmail.com>
2021-02-06 17:47:57 +0000

    basic auth added - login page, login, authentication

---
+++ nandoku/src/pages/login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginButton from "../../components/loginButton";
+
+function Login() {
+  return (
+    <div>
+      <h1>ナンドクにようこそ</h1>
+      <LoginButton />
+    </div>
+  );
+}
+
+export default Login;
