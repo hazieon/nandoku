d8ee0ada3351c997f755047c6d86bf50c32d2927
Hazieon <handrew3721@gmail.com>
2021-02-08 15:31:30 +0000

    removed unused vars

--- nandoku/src/pages/flashcard/index.js
+++ nandoku/src/pages/flashcard/index.js
@@ -4,38 +4,9 @@ import { Link } from "react-router-dom";
 import Title from "../../components/heading";
 import { useParams } from "react-router-dom";
 import { categories } from "../../components/menu/categories";
-import { Button, Select, Box, Text, Center, VStack } from "@chakra-ui/react";
-import {
-  MdArrowDropDown,
-  PhoneIcon,
-  AddIcon,
-  WarningIcon,
-} from "@chakra-ui/icons";
-const shuffle = require("shuffle-array");
+import { Button, Select } from "@chakra-ui/react";
 
-// let title = "花";
-// let kanji = [
-//   { kanji: "薔薇", yomi: "ばら" },
-//   { kanji: "躑躅", yomi: "つつじ" },
-//   { kanji: "薊", yomi: "あざみ" },
-//   { kanji: "蒲公英", yomi: "たんぽぽ" },
-//   { kanji: "蘭", yomi: "らん" },
-//   { kanji: "菫", yomi: "すみれ" },
-//   { kanji: "鬱金香", yomi: "うっこんこう" },
-//   { kanji: "紫陽花", yomi: "あじさい" },
-//   { kanji: "菖蒲", yomi: "あやめ" },
-//   { kanji: "花梨", yomi: "かりん" },
-//   { kanji: "酸漿", yomi: "ほおずき" },
-//   { kanji: "百合", yomi: "ゆり" },
-//   { kanji: "秋桜", yomi: "こすもす" },
-//   { kanji: "桔梗", yomi: "ききょう" },
-//   { kanji: "百日紅", yomi: "さるすべり" },
-//   { kanji: "水芭蕉", yomi: "みずばしょう" },
-//   { kanji: "椿", yomi: "つばき" },
-//   { kanji: "萩", yomi: "はぎ" },
-//   { kanji: "向日葵", yomi: "ひまわり" },
-//   { kanji: "撫子", yomi: "なでしこ" },
-// ];
+const shuffle = require("shuffle-array");
 
 //bring in kanji as props
 //on button press (start/next), display a random kanji from the array
