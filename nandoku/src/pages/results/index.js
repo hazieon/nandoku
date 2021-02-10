a0271dc4d79632771509aa758207868ea8c8e8b6
Hazieon <handrew3721@gmail.com>
2021-02-10 14:42:09 +0000

    cleaned up a bit

--- nandoku/src/pages/results/index.js
+++ nandoku/src/pages/results/index.js
@@ -1,11 +1,19 @@
 import styles from "./index.module.css";
 
-function Results() {
+function Results({ title, roundSet }) {
+  console.log({ title });
+  console.log({ roundSet });
   return (
     <div className={styles.container}>
-      {/* <div>
-            <Title text={title}></Title>
-          </div> */}
+      <div>
+        <h1 className={styles.flashcard}>results go here</h1>
+        <ul>{title ? title : ""}</ul>
+        {roundSet
+          ? roundSet.map((k) => {
+              return <li>{k}</li>;
+            })
+          : ""}
+      </div>
     </div>
   );
 }
